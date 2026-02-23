17c65c74fa6bdd8b28a1246e5e08e6b2a18e29dd
Christian Letourneau <christian.letourneau@dev.com>
2026-02-23 09:43:18 -0500

    feat: add getAllRepositories function for enhanced repository fetching

--- api/src/routes/github/search/repositories.ts
+++ api/src/routes/github/search/repositories.ts
@@ -1,4 +1,5 @@
 import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods'
+import { Octokit } from '@octokit/rest'
 import { RequestHandler } from 'express'
 import { OctokitClient } from '../../../clients/octokit-client'
 import { getRepositories } from '../../../utilities/github/getRepositories'
@@ -35,4 +36,21 @@ export const repositories: RequestHandler = async (
         items: pullRequests,
         totalCount: data.total_count
     })
+}
+
+const getAllRepositories = async (octokit: Octokit, page: number, q: string) => {
+    const response = {
+        items: [],
+        total: 0
+    }
+    const { data: initialFetch } = await octokit.request('GET /search/repositories', {
+        sort: 'updated',
+        per_page: 100,
+        page: 1,
+        q
+    })
+
+    if (initialFetch.total_count > 100) {
+        const pages = initialFetch.total_count / 100
+    }
 }
